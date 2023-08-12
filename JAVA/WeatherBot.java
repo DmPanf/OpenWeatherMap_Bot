9d8ea67863c4f7a82777ddfa6ff74cede7b6dbd5
Dmitrii Panfilov <99917230+DmPanf@users.noreply.github.com>
2023-08-12 04:05:59 +0300

    Update WeatherBot.java

--- JAVA/WeatherBot.java
+++ JAVA/WeatherBot.java
@@ -3,67 +3,34 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Scanner;
-
 public class WeatherBot extends TelegramLongPollingBot {
-    private static final String TOKEN = "YOUR_TELEGRAM_BOT_TOKEN";
-    private static final String BOT_USERNAME = "YourBotUsername";
-    private static final String WEATHER_API_KEY = "YOUR_OPENWEATHERMAP_API_KEY";
+    @Override
+    public String getBotUsername() {
+        return "YOUR_BOT_USERNAME";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "YOUR_BOT_TOKEN";
+    }
 
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
-            String cityName = update.getMessage().getText();
-            String weatherData = fetchWeatherData(cityName);
-            String response = parseWeatherData(weatherData);
-
-            SendMessage message = new SendMessage()
-                    .setChatId(update.getMessage().getChatId())
-                    .setText(response);
+            // TODO: Get the weather information for update.getMessage().getText() and send it to the chat.
+            String message = "Here is your weather!";
+            SendMessage response = new SendMessage();
+            response.setChatId(update.getMessage().getChatId().toString());
+            response.setText(message);
             try {
-                execute(message);
+                execute(response);
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    private String fetchWeatherData(String city) {
-        try {
-            URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + WEATHER_API_KEY);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            Scanner scanner = new Scanner(connection.getInputStream());
-            StringBuilder result = new StringBuilder();
-            while (scanner.hasNext()) {
-                result.append(scanner.nextLine());
-            }
-            scanner.close();
-            return result.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    private String parseWeatherData(String data) {
-        // Здесь вы можете проанализировать JSON ответ и вернуть пользователю нужную информацию о погоде.
-        // Просто для демонстрации, мы вернем всю строку.
-        return data;
-    }
-
-    @Override
-    public String getBotUsername() {
-        return BOT_USERNAME;
-    }
-
-    @Override
-    public String getBotToken() {
-        return TOKEN;
-    }
-
     public static void main(String[] args) {
-        // Запуск бота
+        // TODO: Initialize and start the bot
     }
 }
